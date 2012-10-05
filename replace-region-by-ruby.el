2144f811ec40d68b94c4f882c3eac13deefb4e97
pokutuna <popopopopokutuna@gmail.com>
2012-10-06 03:07:35 +0900

    set STDOUT encoding

--- replace-region-by-ruby.el
+++ replace-region-by-ruby.el
@@ -20,16 +20,27 @@
     (dolist (pat replace-pat buf-string)
       (setq buf-string (replace-regexp-in-string (car pat) (cdr pat) buf-string t)))))
 
-(defun rrbruby:magick-comment-encoding ()
+(defun rrbruby:get-encoding ()
   "thanks to http://d.hatena.ne.jp/rubikitch/20080307/magiccomment"
-  (let* ((coding-system (symbol-name buffer-file-coding-system))
-         (encoding (cond ((string-match "japanese-iso-8bit\\|euc-j" coding-system)
-                          "euc-jp")
-                         ((string-match "shift.jis\\|sjis\\|cp932" coding-system)
-                          "shift_jis")
-                         ((string-match "utf-8" coding-system)
-                          "utf-8"))))
-    (format "# -*- coding: %s -*-" encoding)))
+  (let* ((coding-system (symbol-name buffer-file-coding-system)))
+    (cond ((string-match "japanese-iso-8bit\\|euc-j" coding-system)
+           "euc-jp")
+          ((string-match "shift.jis\\|sjis\\|cp932" coding-system)
+           "shift_jis")
+          ((string-match "utf-8" coding-system)
+           "utf-8"))))
+
+(defun rrbruby:magick-comment (encoding)
+  (cond (encoding
+         (format "# -*- coding: %s -*-" encoding))
+        (t
+         "")))
+
+(defun rrbruby:set-external-encoding-ruby (encoding)
+  (cond (encoding
+         (format "STDOUT.set_encoding('%s')" encoding))
+        (t
+         "")))
 
 (defun replace-region-by-ruby (start end expr)
   (interactive (list (region-beginning) (region-end) (rrbruby:read-string)))
@@ -37,10 +48,12 @@
   (let ((script (make-temp-name (expand-file-name "rrbruby" temporary-file-directory)))
         (stderr (make-temp-name (expand-file-name "rrbrerr" temporary-file-directory)))
         (region (buffer-substring start end))
+        (encoding (rrbruby:get-encoding))
         (coding-system-for-read buffer-file-coding-system) ;; reading for external process output
         (status))
-    (write-region (format "%s\n%s='%s'; %s"
-                          (rrbruby:magick-comment-encoding)
+    (write-region (format "%s\n%s\n%s='%s'; %s"
+                          (rrbruby:magick-comment encoding)
+                          (rrbruby:set-external-encoding-ruby encoding)
                           rrbruby:region-variable
                           (rrbruby:escape-for-rubystring region) expr) nil script)
     (setq status (call-process-region
@@ -53,8 +66,8 @@
           (goto-char (point-min))
           (message (buffer-substring (search-forward ":") (point-max))))
         (kill-buffer errbuf)))
-    (delete-file script)
-    (delete-file stderr)
+    ;; (delete-file script)
+    ;; (delete-file stderr)
     ))
 
 (defalias 'rrbruby (symbol-function 'replace-region-by-ruby))
